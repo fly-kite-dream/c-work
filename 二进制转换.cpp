7406227ad1c4f1849fe101ae6b3559251f2a5246
张成 <cheng122688@163.com>
2025-01-11 10:36:15 +0800

    Update 二进制转换.cpp

--- 二进制转换.cpp
+++ 二进制转换.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main()
 {
 	char num[100];
-	strcpy_s(num, "2022");
+	strcpy_s(num, "2022");//这里以9进制的2022做实例，把其看作数组
 	int len = strlen(num);
 	int goal = 0;
 	for (int i = 0; i < strlen(num); i++)
 	{
-		goal += (num[i]-'0') * pow(9, len-1);
+		goal += (num[i]-'0') * pow(9, len-1);//注意平方数是从0开始递增的，所以记得-1
 		len--;
 	}
 	printf("%d", goal);
